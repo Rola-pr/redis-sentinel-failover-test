b554bf75d8f0aebc7eb295f24e2fa7065a15e084
Rola <ook1780630095@gmail.com>
2022-11-05 01:17:05 +0800

    Changes

--- src/main/java/Sortion/fogJump.java
+++ src/main/java/Sortion/fogJump.java
@@ -12,7 +12,8 @@ public class fogJump {
         Scanner input = new Scanner(System.in);
         System.out.println("请输入青蛙跳台阶的阶数：");
         int n = input.nextInt();
-        System.out.print("总共有" + jump(n) + "种方法");
+        System.out.println("总共有" + jump(n) + "种方法");
+        System.out.print("递归求出总共有" + jump1(n) + "种方法");
     }
 
     public static int jump(int n){
@@ -23,5 +24,17 @@ public class fogJump {
         for (int i = 2; i <= n; i++)
             arr[i] = arr[i - 1] + arr[i - 2];
         return arr[n];
+    }
+
+
+    /**
+     * 递归方法
+     * @param n
+     * @return
+     */
+    public static int jump1(int n){
+        if (n == 0 || n == 1) return 1;
+        return jump(n - 1) + jump(n - 2);
+
     }
 }
