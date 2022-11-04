2efca32e0964f4fd39ce4eb5fc8dbda7e67a1831
Rola <ook1780630095@gmail.com>
2022-11-05 01:39:05 +0800

    Changes

--- src/main/java/Sortion/breakSearch.java
+++ src/main/java/Sortion/breakSearch.java
@@ -4,13 +4,14 @@ public class breakSearch {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         //int target = 8;
-        System.out.println(breaksearch(arr, 4));
+        System.out.println("所要找的数在第" + breaksearch(arr, 4) + "+1位");
 
-        System.out.println(search(arr,6));
+        System.out.println("所要找的数在第" + search(arr,6) + "+1位");
 
+        System.out.println("所要找的数在第" +
+                binarySearch(arr,0,arr.length - 1, 10) + "+1位");
         //breaksearch(arr, target);
     }
-
 
     private static int breaksearch(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++)
@@ -41,4 +42,21 @@ public class breakSearch {
         return -1;
     }
 
+    /**
+     * 改进版本二分搜索
+     * 使用递归
+     */
+
+    public static int binarySearch(int[] arr, int left, int right, int target){
+
+        if (left > right) return -1;
+        int mid = (left + right) / 2;
+
+        if(arr[mid] > target)
+            return binarySearch(arr, left,mid -1, target);
+        else if(arr[mid] < target)
+            return binarySearch(arr, mid + 1, right, target);
+        else
+            return mid;
+    }
 }
