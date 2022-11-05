7407c705e73f0b64f65e91233d1efdb95d120d2a
Peirong Zhang <y.peirong.zhang@gmail.com>
2022-11-06 00:21:42 +0800

    Changes

--- src/main/java/Sortion/threadUse.java
+++ src/main/java/Sortion/threadUse.java
@@ -1,12 +1,14 @@
 package Sortion;
 
+import java.util.Timer;
+
 public class threadUse {
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(()->{
             System.out.println("线程 1 开始执行！");
             for(int i = 0; i < 100; i++){
                 System.out.print("1打印 " + i + " ");
-                if(i % 20 == 0) System.out.println();
+                if(i % 15 == 0) System.out.println();
             }
         });
 
@@ -14,10 +16,11 @@ public class threadUse {
             System.out.println("线程 2 开始执行！");
 
             for (int i = 0; i < 50; i++){
-                System.out.println("2打印 " + i + " ");
+                System.out.print("2打印 " + i + " ");
+                if (i % 15 == 0) System.out.println();
                 if (i == 10)
                     try {
-                        System.out.println("线程 1 加入");
+                        System.out.println("\n线程 1 加入\n");
                         thread.join();
                     }catch (InterruptedException e){
                         e.printStackTrace();
@@ -32,9 +35,11 @@ public class threadUse {
 
         Thread t1 = new Thread(()->{
             synchronized (o) {
+                System.out.println("\n我是线程t1: ");
                     try {
                         for (int i = 0; i < 50; i++) {
                             System.out.print(i + " ");
+                            if(i == 20) System.out.println();
                         }
                         o.wait();
                     } catch (InterruptedException e) {
@@ -47,18 +52,20 @@ public class threadUse {
 
         Thread t2 = new Thread(()->{
             synchronized (o) {
+                System.out.println("\n我是线程t2: ");
                 for (int i = 0; i < 50; i++) {
                     System.out.print(i + " ");
+                    if (i == 20) System.out.println();
                 }
                 o.notify();
             }
         });
 
         t1.start();
-
-
         t2.start();
 
-        //o.notify();
+        Timer timer = new Timer();
+
+
     }
 }
